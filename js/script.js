dec0095602b40bbe5fd5645bdac84fb491f0ba58
palumbolorenzo <palumbolorenzo97@gmail.com>
2022-11-02 17:06:06 +0100

    provvisorio

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 const eleGrid = document.querySelector('.grid');
 
 
-for (let i = 0; i < 100; i++) {
+for (let i = 1; i < 101; i++) {
   //eleGrid.innerHTML += '<div class="cell" style=""></div>';
 
   const eleCell = document.createElement('div');
   eleCell.classList.add('cell');
   eleGrid.append(eleCell);
+  eleCell.append([i]);
 
   eleCell.addEventListener('click', function () {
     this.classList.toggle('active')
