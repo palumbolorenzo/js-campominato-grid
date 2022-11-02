d9e71158591c9a932de51d204711fd006a76c4de
palumbolorenzo <palumbolorenzo97@gmail.com>
2022-11-02 14:16:14 +0100

    provvisorio

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const eleGrid = document.querySelector('.grid');
 
 
-for (let i = 0; i < 64; i++) {
+for (let i = 0; i < 100; i++) {
   //eleGrid.innerHTML += '<div class="cell" style=""></div>';
 
   const eleCell = document.createElement('div');
