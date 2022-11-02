c5f0254535634f1202c87300a8ebc204e21c1bcb
palumbolorenzo <palumbolorenzo97@gmail.com>
2022-11-02 14:10:46 +0100

    files created

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const eleGrid = document.querySelector('.grid');
+
+
+for (let i = 0; i < 64; i++) {
+  //eleGrid.innerHTML += '<div class="cell" style=""></div>';
+
+  const eleCell = document.createElement('div');
+  eleCell.classList.add('cell');
+  eleGrid.append(eleCell);
+
+  eleCell.addEventListener('click', function () {
+    this.classList.toggle('active')
+  });
+}
